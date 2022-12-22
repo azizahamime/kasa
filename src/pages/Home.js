69092b7e253b3d0a88533be0fca45fb404746def
Aziza H <mess.aziza@gmail.com>
2022-12-22 10:19:50 +0100

    initialisation du projet kasa et organisation des fichiers

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import '../style/Home.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello !!</h1>
+      <p> Juste un teste</p>
+    </div>
+  );
+}
+
+export default App;
