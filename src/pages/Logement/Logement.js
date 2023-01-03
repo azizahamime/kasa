221b79f537ac0480414169110b9d07990dbb8aa1
Aziza H <mess.aziza@gmail.com>
2023-01-03 06:23:16 +0100

    Ajouter le composant tag , et modifier la page logement

--- src/pages/Logement/Logement.js
+++ src/pages/Logement/Logement.js
@@ -1,15 +1,32 @@
 
-import React from 'react';
+import React, { Fragment } from 'react';
 import { useParams } from 'react-router-dom';
 import Carrousel from '../../components/Carrousel/Carrousel';
+import Tag from '../../components/Tags/Tag';
 
 export default function Logement(props) {
 	const { logId } = useParams();
 	const location = props.locationList.filter((el) => el.id === logId).shift();
 	console.log(location.pictures);
 	return (
-
-		<Carrousel images={location.pictures} title={location.title} />
+		<Fragment>
+			<Carrousel images={location.pictures} title={location.title} />
+			<div className='information'>
+				<div className='container'>
+					<div className='house'>
+						<h1 className='house-title'> {location.title}</h1>
+						<p className='house-location'> {location.location}</p>
+						<Tag tags={location.tags} />
+					</div>
+					<div className='host'>
+						<div className='host-infos'>
+							<p className='host-infos__name'>{location.host.name}</p>
+							<img src={location.host.picture} alt={location.host.name} className="host-infos__photo" />
+						</div>
+					</div>
+				</div>
+			</div>
+		</Fragment>
 	);
 
 
